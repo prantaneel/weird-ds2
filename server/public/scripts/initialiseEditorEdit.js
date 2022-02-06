252a3069cf837893784f8d7744c80c62491fefb4
soulado <prantaneel2001@gmail.com>
2022-02-06 17:26:06 +0530

    commit6

--- server/public/scripts/initialiseEditorEdit.js
+++ server/public/scripts/initialiseEditorEdit.js
@@ -41,6 +41,121 @@ tinymce.init({
   external_plugins: {
     tiny_mce_wiris: "https://www.wiris.net/demo/plugins/tiny_mce/plugin.js",
   },
+  // enable title field in the Image dialog
+  image_title: true,
+  // enable automatic uploads of images represented by blob or data URIs
+  automatic_uploads: true,
+  // add custom filepicker only to Image dialog
+  file_picker_types: "image",
+  file_picker_callback: function (cb, value, meta) {
+    var input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    function getImageDimensions(image) {
+      return new Promise((resolve, reject) => {
+        image.onload = function (e) {
+          const width = this.width;
+          const height = this.height;
+          resolve({ height, width });
+        };
+      });
+    }
+    function compressImage(image, scale, initalWidth, initalHeight) {
+      return new Promise((resolve, reject) => {
+        const canvas = document.createElement("canvas");
+
+        canvas.width = scale * initalWidth;
+        canvas.height = scale * initalHeight;
+
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+        ctx.canvas.toBlob((blob) => {
+          resolve(blob);
+        }, "image/png");
+      });
+    }
+    input.onchange = async function () {
+      var file = this.files[0];
+      var reader = new FileReader();
+      const uploadedImage = file;
+      const inputPreview = document.createElement("img");
+      inputPreview.src = URL.createObjectURL(uploadedImage);
+
+      //get the dimensions of the input image
+      var { height, width } = await getImageDimensions(inputPreview);
+      const MAX_WIDTH = 720; //if we resize by width, this is the max width of compressed image
+      const MAX_HEIGHT = (height / width + 1) * MAX_WIDTH;
+      const widthRatioBlob = await compressImage(
+        inputPreview,
+        MAX_WIDTH / width,
+        width,
+        height
+      );
+      const heightRatioBlob = await compressImage(
+        inputPreview,
+        MAX_HEIGHT / height,
+        width,
+        height
+      );
+
+      //pick the smaller blob between both
+      const compressedBlob =
+        widthRatioBlob.size > heightRatioBlob.size
+          ? heightRatioBlob
+          : widthRatioBlob;
+      const optimalBlob =
+        compressedBlob.size < uploadedImage.size
+          ? compressedBlob
+          : uploadedImage;
+      if (optimalBlob === compressedBlob) {
+        var scale;
+        if (compressedBlob === heightRatioBlob) {
+          scale = MAX_HEIGHT / height;
+          height = MAX_HEIGHT;
+          width = scale * width;
+        }
+        if (compressedBlob === widthRatioBlob) {
+          scale = MAX_WIDTH / width;
+          width = MAX_WIDTH;
+          height = scale * height;
+        }
+      }
+      //converting from blob to base 64
+      var reader2 = new FileReader();
+      reader2.readAsDataURL(optimalBlob);
+      var base64String;
+      reader2.onloadend = function () {
+        base64String = reader2.result;
+        img = base64String;
+        // var check = base64String.substr(0, 23);
+        // var IND;
+        // if (check.indexOf("png") === -1) IND = 23;
+        // else IND = 22;
+        // var sub = base64String.substr(IND);
+        // img = sub;
+      };
+
+      reader.onload = function () {
+        var id = "blobid" + new Date().getTime();
+        var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+        var base64 = base64String.split(",")[1];
+        console.log(base64String);
+        var blobInfo = blobCache.create(id, file, base64);
+        blobCache.add(blobInfo);
+        console.log(blobCache);
+        // call the callback and populate the Title field with the file name
+        cb(blobInfo.blobUri(), {
+          title: file.name,
+          width: `${width}`,
+          height: `${height}`,
+        });
+      };
+      reader.readAsDataURL(file);
+    };
+
+    input.click();
+  },
   menubar: "favs file edit view insert format tools table help",
   content_css: "/styles/content.css",
   codesample_content_css: "prism/prism.css",
